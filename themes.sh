6d747f0315f1a32139d50fc35a0962d612a5daf9
BigAnteater <walterjamesarron@gmail.com>
2022-04-18 17:33:48 -0700

    seperate script for themes

---
+++ themes.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "This script will install my config files."
+sleep 3s
+clear
+
+# Installs themes
+echo "First we will be installing the GTK themes into ~/.rice" 
+sleep 2s
+
+mkdir ~/.themes
+cd ~/.themes
+
+# GTK Theme
+git clone https://github.com/vinceliuice/Orchis-theme
+cd Orchis-theme
+bash install.sh -t purple
+
+# GTK Icons Theme
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme
+cd Tela-circle-icon-theme
+bash install.sh purple
+
+
+# Checks that ~/.config exists, then copies it over.
+cd ~
+
+if [ -d ".config" ]
+then
+        echo "looks like .config exists. Well I hope you don't mind me overwriting it."
+	rm -rf .config
+	cp -r ~/Arch-setup/config ~/.config
+else
+        echo "nope... creating .config"
+	cp -r ~/Arch-setup/config/ ~/.config
+fi
+
+# Installs wallpaper themes.
+cp -r ~/Arch-setup/wallpapers/ ~/.wallpapers
+
+sleep 2s
+
+echo "customization has been configured!"
+clear
